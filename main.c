62529922f576843bbc36c859c3427e7ac3969588
Mostafa Elkhokhy <147348000+elkhokh@users.noreply.github.com>
2024-10-04 11:10:32 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ * DIO_DRIVER.c
+ *
+ * Created: 13/07/2024 03:20:24
+ * Author : MUSTAFA_KHALID
+ */ 
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "DIO_Interface.h"
+
+int main(void)
+{
+	DIO_voidSetPinDirection(PORTA,PIN5,OUTPUT);
+	while (1)
+	{
+	}
+}
+
+
